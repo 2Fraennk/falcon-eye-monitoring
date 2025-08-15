5fe19b9aead3566846874b939dcb9b09c4817464
Frank Birkhofen <frank.birkhofen@telekom.de>
2025-08-15 11:08:56 +0200

    update notification in test script

--- tests/src/tests.sh
+++ tests/src/tests.sh
@@ -3,7 +3,7 @@ poetry run pytest --verbose /tests/src/test/test_demoapp.py
 rc=$?
 if [ "$rc" -gt 0 ]; then exit 1; fi
 
-echo "Notice: waiting for metrics, logs and traces up to 60 seconds being shipped and calculated"
+echo "!!! Notice: waiting for metrics, logs and traces up to 60 seconds being shipped and calculated"
 
 poetry run pytest --verbose /tests/src/test/test_grafana_demoapp.py
 rc=$?
